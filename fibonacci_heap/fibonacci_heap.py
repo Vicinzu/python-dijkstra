161fe64667da6f5d766e75bba89d48d8bab2e0ab
vicinzu <don.vicinzu@gmail.com>
2020-09-15 21:23:05 +0200

    Enhance the fibonacci heap implementation

--- fibonacci_heap/fibonacci_heap.py
+++ fibonacci_heap/fibonacci_heap.py
@@ -1,143 +1,252 @@
 from dataclasses import dataclass
-from math import inf
-from typing import List, Tuple
+from math import ceil, inf, log
+from typing import Generic, TypeVar
+
+FibonacciHeapNodeItemType = TypeVar('FibonacciHeapNodeItemType')
 
 
-@dataclass
-class _FibonacciNode:
-    priority: float
-    item: object
-    parent: '_FibonacciNode'
-    firstChild: '_FibonacciNode'
-    leftSibling: '_FibonacciNode'
-    rightSibling: '_FibonacciNode'
-    rank: int
-    mark: bool
+class FibonacciHeapNode(Generic[FibonacciHeapNodeItemType]):
+    _priority: float
+    _item: FibonacciHeapNodeItemType
+    _parent: 'FibonacciHeapNode'
+    _firstChild: 'FibonacciHeapNode'
+    _leftSibling: 'FibonacciHeapNode'
+    _rightSibling: 'FibonacciHeapNode'
+    _rank: int
+    _mark: bool
 
-    def __init__(self, priority: float, item: object = None, parent: '_FibonacciNode' = None):
-        self.priority = priority
-        self.item = item
-        self.parent = parent
-        self.firstChild = None
-        self.leftSibling = self
-        self.rightSibling = self
-        self.rank = 0
-        self.mark = False
+    def __init__(self, priority: float, item: FibonacciHeapNodeItemType = None):
+        self._priority = priority
+        self._item = item
+        self._parent = None
+        self._firstChild = None
+        self._leftSibling = self
+        self._rightSibling = self
+        self._rank = 0
+        self._mark = False
 
+    def __str__(self):
+        parentName = ''
+        if self._parent is not None:
+            parentName = self._parent._item
+        return 'FibonacciHeapNode(priority={:d}, item={}, rank={:d}, mark={}, parent={})'.format(self._priority, self._item, self._rank, self._mark, parentName)
 
-class FibonacciHeap:
-    __root: _FibonacciNode
-    __minNode: _FibonacciNode
-    __length: int
+    def __repr__(self):
+        return self.__str__()
 
-    def __init__(self):
-        self.__root = None
-        self.__minNode = None
-        self.__length = 0
+    def __iter__(self):
+        return _FibonacciHeapNodeIterator(self)
 
     def __len__(self):
-        return self.__length
+        return self._rank
 
-    def add(self, priority: float, item: object = None):
-        newTreeNode: _FibonacciNode = _FibonacciNode(priority, item)
-        self.__merge_into_root(newTreeNode)
-        if self.__minNode == None or newTreeNode.priority <= self.__minNode.priority:
-            self.__minNode = newTreeNode
-        self.__length += 1
+    def getPriority(self):
+        return self._priority
 
-    def getMin(self) -> Tuple[float, object]:
-        if self.__minNode == None:
-            return (inf, None)
+    def getItem(self):
+        return self._item
 
-        return (self.__minNode.priority, self.__minNode.item)
 
-    def extractMin(self) -> Tuple[float, object]:
-        if self.__minNode == None:
-            return (inf, None)
+class FibonacciHeap(Generic[FibonacciHeapNodeItemType]):
+    _root: FibonacciHeapNode
+    _minNode: FibonacciHeapNode
+    _length: int
 
-        result: Tuple[float, object] = (self.__minNode.priority, self.__minNode.item)
-        self.__cut_out_root(self.__minNode)
-        self.__set_new_min()
-        self.__length -= 1
+    def __init__(self):
+        self._root = None
+        self._minNode = None
+        self._length = 0
+
+    def __str__(self):
+        result: str = ''
+        for rootNode in self:
+            result += self.__printNode(rootNode)
         return result
 
-    def decreaseKey(self, new_priority: float, old_priority: float, item: object = None):
-        treeNode: _FibonacciNode = self.__find_key_value_internal(self.__root, old_priority, item)
-
-        if treeNode is not None:
-            treeNode.priority = new_priority
-            if(treeNode.parent is not None and treeNode.parent.priority > new_priority):
-                self.__cut_out_node(treeNode)
-                self.__merge_into_root(treeNode)
-            if treeNode.priority < self.__minNode.priority:
-                self.__minNode = treeNode
-
-    def __find_key_value(self, priority: float, item: object = None) -> _FibonacciNode:
-        return self.__find_key_value_internal(self.__root, priority, item)
-
-    def __find_key_value_internal(self, startNode: _FibonacciNode, priority: float, item: object = None) -> _FibonacciNode:
-        result = None
-
-        if startNode is not None:
-            currentNode: _FibonacciNode = startNode
-            while True:
-                if currentNode.priority == priority and currentNode.item == item:
-                    result = currentNode
-                elif currentNode.priority < priority:
-                    result = self.__find_key_value_internal(currentNode, priority, item)
-
-                if result is not None or currentNode.rightSibling == self.__root:
-                    break
-                else:
-                    currentNode = currentNode.rightSibling
+    @classmethod
+    def __printNode(cls, node: FibonacciHeapNode, level: int = 0) -> str:
+        result: str = '{:=2d}:\t'.format(level)+"\t"*level+str(node)+"\n"
+        for childnode in node:
+            result += cls.__printNode(childnode, level+1)
 
         return result
 
-    def __set_new_min(self):
-        self.__minNode = self.__root
-        if(self.__root is None):
+    def __iter__(self):
+        return _FibonacciHeapIterator(self)
+
+    def __len__(self):
+        return self._length
+
+    def add(self, priority: float, item: FibonacciHeapNodeItemType = None) -> FibonacciHeapNode[FibonacciHeapNodeItemType]:
+        newnode: FibonacciHeapNode = FibonacciHeapNode(priority, item)
+        self.__add_nodes_as_roots(newnode)
+        if self._minNode == None or newnode._priority <= self._minNode._priority:
+            self._minNode = newnode
+        self._length += 1
+
+        return newnode
+
+    def getMin(self) -> FibonacciHeapNode[FibonacciHeapNodeItemType]:
+        return self._minNode
+
+    def extractMin(self) -> FibonacciHeapNode[FibonacciHeapNodeItemType]:
+        result: FibonacciHeapNode[FibonacciHeapNodeItemType] = self._minNode
+        if result is not None:
+            self.__cutoff_node(result)
+            self.__add_nodes_as_roots(result._firstChild)
+            self._length -= 1
+            self._minNode = None
+            self.__cleanup_roots()
+
+        return result
+
+    def decreaseKey(self, new_priority: float, node: FibonacciHeapNode[FibonacciHeapNodeItemType]):
+        node._priority = new_priority
+        self.__update_min(node)
+
+        parent: FibonacciHeapNode = node._parent
+        if parent is not None and parent._priority > node._priority:
+            self.__cutoff_node(node)
+            self.__add_nodes_as_roots(node)
+            self.__cutoff_node_recursive(parent)
+
+    def __update_min(self, node: FibonacciHeapNode[FibonacciHeapNodeItemType]):
+        if self._minNode is None or self._minNode._priority > node._priority:
+            self._minNode = node
+
+    def __add_nodes_as_roots(self, newRoot: FibonacciHeapNode[FibonacciHeapNodeItemType]):
+        if newRoot is None:
             return
 
-        currentNode: _FibonacciNode = self.__root
-        while not currentNode.rightSibling == self.__root:
-            currentNode = currentNode.rightSibling
-            if currentNode.priority <= self.__root.priority:
-                self.__minNode = currentNode
-
-    def __merge_trees(self, leftTreeRoot: _FibonacciNode, rightTreeRoot: _FibonacciNode):
-        rightTreeRoot.leftSibling = leftTreeRoot.leftSibling
-        rightTreeRoot.rightSibling = leftTreeRoot
-        leftTreeRoot.leftSibling.rightSibling = rightTreeRoot
-        leftTreeRoot.leftSibling = rightTreeRoot
-
-    def __merge_into_root(self, newTreeRoot: _FibonacciNode):
-        if(self.__root is None):
-            self.__root = newTreeRoot
+        newRoot._parent = None
+        if(self._root is None):
+            self._root = newRoot
         else:
-            self.__merge_trees(self.__root, newTreeRoot)
+            currentLastElement:  FibonacciHeapNode = self._root._leftSibling
+            self._root._leftSibling = newRoot._leftSibling
+            newRoot._leftSibling._rightSibling = self._root
+            newRoot._leftSibling = currentLastElement
+            currentLastElement._rightSibling = newRoot
 
-    def __add_children_as_roots(self, treeRoot: _FibonacciNode):
-        firstChild: _FibonacciNode = treeRoot.firstChild
-        if firstChild is not None:
-            treeRoot.rightSibling.leftSibling = firstChild.leftSibling
-            firstChild.leftSibling.rightSibling = treeRoot
-            firstChild.leftSibling = treeRoot
-            treeRoot.rightSibling = firstChild
-            firstChild.parent = None
+    def __cleanup_roots(self):
+        if self._root is None:
+            return
 
-    def __cut_out_node(self, treeNode: _FibonacciNode):
-        if not treeNode.rightSibling == treeNode:
-            rightSibling = treeNode.rightSibling
-            rightSibling.leftSibling = treeNode.leftSibling
-            treeNode.leftSibling.rightSibling = rightSibling
-            treeNode.leftSibling = treeNode.rightSibling = treeNode
+        maxRank: int = 1+ceil(2*log(self._length))
+        sortField: List[FibonacciHeapNode[FibonacciHeapNodeItemType]] = [None for f in range(maxRank)]
+        while self._root is not None:
+            currentRoot: FibonacciHeapNode[FibonacciHeapNodeItemType] = self._root
+            currentRank: int = currentRoot._rank
+            self.__cutoff_node(currentRoot)
+            self.__update_min(currentRoot)
+            while sortField[currentRank] is not None:
+                currentRoot = self.__link_nodes(currentRoot, sortField[currentRank])
+                sortField[currentRank] = None
+                currentRank += 1
+            sortField[currentRank] = currentRoot
+        for r in range(maxRank):
+            if sortField[r] is not None:
+                self.__add_nodes_as_roots(sortField[r])
 
-    def __cut_out_root(self, treeRoot: _FibonacciNode):
-        if self.__root == treeRoot:
-            if treeRoot.rightSibling == treeRoot:
-                self.__root = None
+    def __add_node_as_child(self, parent: FibonacciHeapNode[FibonacciHeapNodeItemType], child: FibonacciHeapNode[FibonacciHeapNodeItemType]):
+        if parent._firstChild is None:
+            parent._firstChild = child
+            child._parent = parent
+        else:
+            firstChild: FibonacciHeapNode[FibonacciHeapNodeItemType] = parent._firstChild
+            lastChild: FibonacciHeapNode[FibonacciHeapNodeItemType] = firstChild._leftSibling
+
+            lastChild._rightSibling = child
+            child._leftSibling = lastChild
+            child._rightSibling = firstChild
+            firstChild._leftSibling = child
+
+        parent._rank += 1
+
+    def __cutoff_node(self, node: FibonacciHeapNode[FibonacciHeapNodeItemType]):
+        hasSibling: bool = node._rightSibling != node
+        # If the node has a parent, then some data has to be corrected
+        if node._parent is not None:
+            # Decrease the rank of the parent
+            node._parent._rank -= 1
+            # Update the child pointer
+            if node._parent._firstChild == node:
+                if hasSibling:
+                    node._parent._firstChild = node._rightSibling
+                else:
+                    node._parent._firstChild = None
+            node._parent = None
+        # The root pointer has to be updated, if it marks the node to cut
+        elif self._root == node:
+            if node._rightSibling == node:
+                self._root = None
             else:
-                self.__root = treeRoot.rightSibling
+                self._root = node._rightSibling
 
-        self.__add_children_as_roots(treeRoot)
-        self.__cut_out_node(treeRoot)
+        # if the element has siblings, then cut the element off the sibling list
+        if hasSibling:
+            _rightSibling = node._rightSibling
+            _rightSibling._leftSibling = node._leftSibling
+            node._leftSibling._rightSibling = _rightSibling
+            node._leftSibling = node._rightSibling = node
+
+    def __cutoff_node_recursive(self, node: FibonacciHeapNode):
+        if node is not None:
+            if node._mark == True:
+                self.__cutoff_node(node)
+                self.__add_nodes_as_roots(node)
+                self.__cutoff_node_recursive(node._parent)
+            else:
+                node._mark = True
+
+    def __link_nodes(self, leftTree: FibonacciHeapNode[FibonacciHeapNodeItemType], rightTree: FibonacciHeapNode[FibonacciHeapNodeItemType]) -> FibonacciHeapNode[FibonacciHeapNodeItemType]:
+        parent: FibonacciHeapNode[FibonacciHeapNodeItemType] = leftTree
+        child: FibonacciHeapNode[FibonacciHeapNodeItemType] = leftTree
+        if rightTree._priority < leftTree._priority:
+            parent = rightTree
+        else:
+            child = rightTree
+
+        self.__add_node_as_child(parent, child)
+        child._mark = False
+        return parent
+
+
+class _FibonacciHeapNodeIterator:
+    __firstElement: FibonacciHeapNode
+    __currentElement: FibonacciHeapNode
+    __run: bool
+
+    def __init__(self, fibonacciHeapNode: FibonacciHeapNode):
+        self.__firstElement = fibonacciHeapNode._firstChild
+        self.__currentElement = self.__firstElement
+        self.__run = False
+
+    def __next__(self):
+        if self.__firstElement is None or (self.__run and self.__currentElement == self.__firstElement):
+            raise StopIteration
+
+        result = self.__currentElement
+        self.__currentElement = self.__currentElement._rightSibling
+        self.__run = True
+        return result
+
+
+class _FibonacciHeapIterator:
+    __firstElement: FibonacciHeapNode
+    __currentElement: FibonacciHeapNode
+    __run: bool
+
+    def __init__(self, fibonacciHeap: FibonacciHeap):
+        self.__firstElement = fibonacciHeap._root
+        self.__currentElement = self.__firstElement
+        self.__run = False
+
+    def __next__(self):
+        if self.__currentElement is None or (self.__run and self.__currentElement == self.__firstElement):
+            raise StopIteration
+
+        result = self.__currentElement
+        self.__currentElement = self.__currentElement._rightSibling
+        self.__run = True
+        return result
--- tests/test_fibonacci_heap.py
+++ tests/test_fibonacci_heap.py
@@ -1,40 +1,58 @@
 import unittest
-from typing import Tuple
+from typing import List, Tuple
 
 from fibonacci_heap import *
 
 
 class TestFibonacciHeap(unittest.TestCase):
     @staticmethod
-    def __getFibonacciHeapTestData() -> List[Tuple[float, object]]:
-        return [(10, 'Element 1'), (5, 'Element 2'), (20, 'Element 3'), (25, 'Element 4')]
+    def __getFibonacciHeapTestData() -> List[Tuple[float, str]]:
+        return [(10, 'Element 1'),
+                (5, 'Element 2'),
+                (20, 'Element 3'),
+                (25, 'Element 4')]
 
-    def __testElement(self, gotElement: Tuple[float, object], expectedElement: Tuple[float, object]):
-        self.assertEqual(expectedElement, gotElement)
+    @staticmethod
+    def __genFibonacciHeapTestData(num=10) -> List[Tuple[float, str]]:
+        result: List[Tuple[float, str]] = []
+        for i in range(num):
+            result.append((i, 'Element '+str(i)))
+
+        return result
+
+    def __testElement(self, expectedElement: Tuple[float, str], receivedElement: FibonacciHeapNode[str]):
+        self.assertEqual(expectedElement, self.__toTuple(receivedElement))
+
+    @staticmethod
+    def __toTuple(receivedElement: FibonacciHeapNode[str]) -> Tuple[float, str]:
+        if receivedElement is None:
+            return None
+
+        return (receivedElement.getPriority(), receivedElement.getItem())
 
     def testFibonacciHeap_AddGetMin(self):
-        fheap: FibonacciHeap = FibonacciHeap()
-        minElement: Tuple[float, object] = (inf, None)
+        fheap: FibonacciHeap = FibonacciHeap[str]()
+        minElement: Tuple[float, str] = None
         self.assertEqual(len(fheap), 0)
-        self.__testElement(fheap.getMin(), minElement)
+        self.__testElement(minElement, fheap.getMin())
 
         element = minElement = (10, 'Element 1')
         fheap.add(*element)
         self.assertEqual(len(fheap), 1)
-        self.__testElement(fheap.getMin(), minElement)
+        self.__testElement(minElement, fheap.getMin())
 
         element = (20, 'Element 2')
         fheap.add(*element)
         self.assertEqual(len(fheap), 2)
-        self.__testElement(fheap.getMin(), minElement)
+        self.__testElement(minElement, fheap.getMin())
 
         element = minElement = (5, 'Element 3')
         fheap.add(*element)
         self.assertEqual(len(fheap), 3)
-        self.__testElement(fheap.getMin(), minElement)
+        self.__testElement(minElement, fheap.getMin())
 
     def testFibonacciHeap_AddExtractMin(self):
-        fheap: FibonacciHeap = FibonacciHeap()
+        fheap: FibonacciHeap = FibonacciHeap[str]()
         self.assertEqual(len(fheap), 0)
         testData: List[Tuple[float, object]] = self.__getFibonacciHeapTestData()
 
@@ -45,27 +63,78 @@ class TestFibonacciHeap(unittest.TestCase):
         fheap.add(*testData[2])
         self.assertEqual(len(fheap), 3)
 
-        self.__testElement(fheap.extractMin(), testData[1])
+        self.__testElement(testData[1], fheap.extractMin())
         self.assertEqual(len(fheap), 2)
-        self.__testElement(fheap.extractMin(), testData[0])
+        self.__testElement(testData[0], fheap.extractMin())
         self.assertEqual(len(fheap), 1)
         fheap.add(*testData[3])
         self.assertEqual(len(fheap), 2)
-        self.__testElement(fheap.extractMin(), testData[2])
+        self.__testElement(testData[2], fheap.extractMin())
         self.assertEqual(len(fheap), 1)
-        self.__testElement(fheap.extractMin(), testData[3])
+        self.__testElement(testData[3], fheap.extractMin())
         self.assertEqual(len(fheap), 0)
-        self.__testElement(fheap.extractMin(), (inf, None))
+        self.__testElement(None, fheap.extractMin())
 
-    def testFibonacciHeap_DecreasePriority(self):
-        fheap: FibonacciHeap = FibonacciHeap()
-        testData: List[Tuple[float, object]] = self.__getFibonacciHeapTestData()
+    def testFibonacciHeap_DecreasePriority_Normal(self):
+        fheap: FibonacciHeap = FibonacciHeap[str]()
+        testData: List[Tuple[float, str]] = self.__getFibonacciHeapTestData()
+        testDataEntry: List[FibonacciHeapNode] = []
+        for dataEntry in testData:
+            testDataEntry.append(fheap.add(*dataEntry))
+        self.assertEqual(len(fheap), 4)
+        self.__testElement(testData[1], fheap.extractMin())
 
-        fheap.add(*testData[0])
-        self.assertEqual(len(fheap), 1)
-        fheap.add(*testData[1])
-        self.assertEqual(len(fheap), 2)
+        # Test a decrease that does not change the tree
+        fheap.decreaseKey(15, testDataEntry[2])
+        self.__testElement(testData[0], fheap.getMin())
 
-        self.__testElement(fheap.getMin(), testData[1])
-        fheap.decreaseKey(2, *testData[0])
-        self.__testElement(fheap.getMin(), (2, testData[0][1]))
+        # Test a decrease that changes the tree
+        fheap.decreaseKey(8, testDataEntry[2])
+        self.__testElement((8, testData[2][1]), fheap.getMin())
+
+    def testFibonacciHeap_DecreasePriority_RecursiveCut(self):
+        fheap: FibonacciHeap = FibonacciHeap[str]()
+        testData: List[Tuple[float, str]] = self.__genFibonacciHeapTestData(10)
+        testDataEntry: List[FibonacciHeapNode] = []
+        for dataEntry in testData:
+            testDataEntry.append(fheap.add(*dataEntry))
+        self.assertEqual(len(fheap), 10)
+        self.__testElement(testData[0], fheap.extractMin())
+
+        # Ensure that a parent node is marked first and then cut as root
+        fheap.decreaseKey(4, testDataEntry[6])
+        fheap.decreaseKey(4, testDataEntry[7])
+
+        # Test if the expected result
+        self.__testElement(testData[1], fheap.extractMin())
+
+    def testFibonacciHeap_Iterate(self):
+        fheap: FibonacciHeap = FibonacciHeap[str]()
+
+        # generate the test data
+        numEntries: int = 10
+        testData: List[Tuple[float, str]] = self.__genFibonacciHeapTestData(numEntries)
+
+        # add the test data to the fibonacci heap
+        for data in testData:
+            fheap.add(*data)
+        # test the number of elements in the heap
+        self.assertEqual(len(fheap), numEntries)
+
+        # test if the number of iterations (roots) is equal to the number of inserted nodes
+        n: int = 0
+        for root in fheap:
+            n += 1
+        self.assertEqual(len(fheap), n)
+
+        # make a binomial tree of it
+        fheap.extractMin()
+
+        # get the smallest entry
+        minNode: FibonacciHeapNode = fheap.getMin()
+
+        # test if the number of iterations (children of node) is equal to the rank (length)
+        n = 0
+        for child in minNode:
+            n += 1
+        self.assertEqual(len(minNode), n)
